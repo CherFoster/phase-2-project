08d12db5881bd957e90228515b92189c3c309f55
Cherise Foster <cherise.foster@outlook.com>
2023-05-10 17:51:37 -0600

    added onClick to picture

--- src/ColorCard.js
+++ src/ColorCard.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ColorCard({ color }) {
+    const [showInfo, setShowInfo] = useState(false);
+
+    function handleClick() {
+        setShowInfo(!showInfo)
+      }
+
     return (
         <div className="color-card">
             <h3>{color.name}</h3>
-            <img className="picture" src={color.image} onClick={() => console.log('clicked')}/>
-            <h4>{color.description}</h4>
-            <p>{color.numbers}</p>
+            <img className="picture" src={color.image} alt="colors" onClick={handleClick}/>
+        
+            {showInfo && (
+            <div>
+             <h4>{color.description}</h4>
+             <p>{color.numbers}</p>
+            </div>)}
         </div>
     )
 }
--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -3,12 +3,14 @@ import ColorCard from './ColorCard';
 
 function ColorContainer() {
   const [colors, setColors] = useState([]);
+  const [showInfo, setShowInfo] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8000/colors")
       .then(response => response.json())
       .then(data => setColors(data))
   }, []);
+
 
   return (
     <div>
